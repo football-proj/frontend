c5b7e2a9b51009a065622923f1b985d5b0f04cde
rojojun <zoloman316@gmail.com>
2025-08-18 21:41:14 +0900

    feat: create components for team list

--- src/CardContainer.tsx
+++ src/CardContainer.tsx
@@ -1,5 +1,12 @@
+import {Button} from "@/components/ui/button.tsx";
+
 const CardContainer = () => {
-    return <div>test</div>
+    return (
+        <div>
+            test
+            <Button>test</Button>
+        </div>
+    )
 }
 
 export default CardContainer
