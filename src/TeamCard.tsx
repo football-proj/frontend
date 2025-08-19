f9c94208ec1d8dbdc8d30dfbadd7f8a7bfeeedc5
rojojun <zoloman316@gmail.com>
2025-08-19 23:31:41 +0900

    fix: change components

--- src/TeamCard.tsx
+++ src/TeamCard.tsx
@@ -2,14 +2,14 @@ import type React from "react";
 import { Button } from "./components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
 
-type CardContainerProps = {
+type TeamCardProps = {
     teamname: string;
     teamDescription: string;
     content: string;
     buttonText: string
 }
 
-const TeamCard: React.FC<CardContainerProps> = ({ teamname, teamDescription, content, buttonText }) => {
+const TeamCard: React.FC<TeamCardProps> = ({ teamname, teamDescription, content, buttonText }) => {
     return (
         <div>
             <Card className="w-full max-w-sm w-[30vw] bg-black border-gray-800">
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import CardContainer from "@/CardContainer.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <div>
+      Team List
+    </div>
     <div className='flex flex-row gap-1 p-2 justify-center items-start'>
       {Array.from({ length: 3 }, (_, i) => (
         <CardContainer
